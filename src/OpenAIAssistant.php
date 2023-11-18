966cc8c5aa213865efc881a9259c4005caf1f1f6
Erdum <erdumadnan@gmail.com>
2023-11-18 21:53:22 +0500

    Update assistant_id optional in the constructor, optionally function can be called on a object, assistant create & modify method introduced. Visible methods first in the class sequence.

--- src/OpenAIAssistant.php
+++ src/OpenAIAssistant.php
@@ -14,7 +14,7 @@ class OpenAIAssistant {
 
     public function __construct(
         $api_key,
-        $assistant_id,
+        $assistant_id = null,
         $base_url = 'https://api.openai.com/v1',
         $version_header = 'OpenAI-Beta: assistants=v1'
     )
@@ -23,6 +23,235 @@ class OpenAIAssistant {
         $this->assistant_id = $assistant_id;
         $this->base_url = $base_url;
         $this->version_header = $version_header;
+    }
+
+    public function create_assistant($name, $instructions, $tools)
+    {
+        $response = $this->send_post_request('/assistants', array(
+            'name' => $name,
+            'instructions' => $instructions,
+            'model' => 'gpt-4-1106-preview',
+            'tools' => $tools
+        ));
+
+        if (empty($response['id'])) {
+            throw new Exception('Unable to create a assistant');
+        }
+        $this->assistant_id = $response['id'];
+        return $response['id'];
+    }
+
+    public function modify_assistant($name, $instructions, $tools)
+    {
+        if (!$this->assistant_id) {
+            throw new Exception(
+                'You need to provide a assistant_id or create an assistant.'
+            );
+        }
+
+        $response = $this->send_post_request("/assistants/{$this->assistant_id}", array(
+            'name' => $name,
+            'instructions' => $instructions,
+            'model' => 'gpt-4-1106-preview',
+            'tools' => $tools
+        ));
+
+        if (empty($response['id'])) {
+            throw new Exception('Unable to create a assistant');
+        }
+        $this->assistant_id = $response['id'];
+        return $response['id'];
+    }
+
+    public function list_assistants()
+    {
+        $response = $this->send_get_request('/assistants');
+
+        if (empty($response['data'])) {
+            return array();
+        }
+        return $response['data'];
+    }
+
+    public function create_thread($content, $role = 'user')
+    {
+        $response = $this->send_post_request('/threads', array(
+            'messages' => array(
+                array(
+                    'role' => $role,
+                    'content' => $content
+                )
+            )
+        ));
+
+        if (empty($response['id'])) {
+            throw new Exception('Unable to create a thread');
+        }
+        return $response['id'];
+    }
+
+    public function get_thread($thread_id)
+    {
+        $response = $this->send_get_request("/threads/{$thread_id}");
+
+        if (empty($response['id'])) {
+            throw new Exception('Unable to retrive a thread');
+        }
+        return $response;
+    }
+
+    public function add_message($thread_id, $content, $role = 'user')
+    {
+        // Check if any latest run requires_action
+        // Before adding a new message to the thread
+        $runs = $this->list_runs($thread_id);
+
+        if (count($runs) > 0) {
+            $last_run = $runs[0];
+
+            if ($last_run['status'] == 'requires_action') {
+                $this->has_tool_calls = true;
+                $this->tool_call_id = $last_run['id'];
+                return false;
+            } else {
+                $this->has_tool_calls = false;
+                $this->tool_call_id = null;
+            }
+        }
+
+        $response = $this->send_post_request(
+            "/threads/{$thread_id}/messages",
+            array(
+                'role' => $role,
+                'content' => $content
+            )
+        );
+
+        if (empty($response['id'])) {
+            throw new Exception('Unable to create a message');
+        }
+        return $response['id'];
+    }
+
+    public function get_message($thread_id, $message_id)
+    {
+        $response = $this->send_get_request("/threads/{$thread_id}/messages/{$message_id}");
+
+        if (empty($response['id'])) {
+            throw new Exception('Unable to retrive a message');
+        }
+        return $response;
+    }
+
+    public function list_thread_messages($thread_id)
+    {
+        $response = $this->send_get_request("/threads/{$thread_id}/messages");
+
+        if (empty($response['data'])) {
+            return array();
+        }
+        return $response['data'];
+    }
+
+    public function run_thread($thread_id)
+    {
+        // Check if any latest run requires_action
+        // Before creating and running a new thread
+        $runs = $this->list_runs($thread_id);
+
+        if (count($runs) > 0) {
+            $last_run = $runs[0];
+
+            if ($last_run['status'] == 'requires_action') {
+                $this->has_tool_calls = true;
+                $this->tool_call_id = $last_run['id'];
+                return false;
+            } else {
+                $this->has_tool_calls = false;
+                $this->tool_call_id = null;
+            }
+        }
+
+        $run_id = $this->create_run($thread_id, $this->assistant_id);
+
+        do {
+            sleep(5);
+            $run = $this->get_run($thread_id, $run_id);
+        } while (!(
+            $run['status'] == 'completed'
+            || $run['status'] == 'requires_action'
+        ));
+
+        if ($run['status'] == 'requires_action') {
+            $this->has_tool_calls = true;
+            $this->tool_call_id = $run['id'];
+            return $run['id'];
+        } else if ($run['status'] == 'completed') {
+            return $run['id'];
+        }
+        return false;
+    }
+
+    public function execute_tools(
+        $thread_id,
+        $execution_id,
+        $optional_object = null
+    )
+    {
+        $run = $this->get_run($thread_id, $execution_id);
+        $calls = $run['required_action']['submit_tool_outputs']['tool_calls'];
+        $outputs = array();
+
+        foreach ($calls as $call) {
+            $method_name = $call['function']['name'];
+            $method_args = json_decode($call['function']['arguments'], true);
+            $callable = $optional_object ? 
+                array($optional_object, $method_name) : $method_name;
+
+            if (is_callable($callable)) {
+                $data = call_user_func_array(
+                    $callable,
+                    $method_args
+                );
+                array_push($outputs, array(
+                    'tool_call_id' => $call['id'],
+                    'output' => json_encode($data)
+                ));
+            } else {
+                throw new Exception("Failed to execute tool: The $method_name you provided is not callable");
+            }
+        }
+        $this->has_tool_calls = false;
+        return $outputs;
+    }
+
+    public function submit_tool_outputs($thread_id, $execution_id, $outputs)
+    {
+        $response = $this->send_post_request(
+            "/threads/{$thread_id}/runs/{$execution_id}/submit_tool_outputs",
+            array('tool_outputs' => $outputs)
+        );
+
+        if (empty($response['id'])) {
+            throw new Exception('Unable to submit tool outputs');
+        }
+
+        do {
+            sleep(5);
+            $run = $this->get_run($thread_id, $response['id']);
+        } while (!(
+            $run['status'] == 'completed'
+            || $run['status'] == 'requires_action'
+        ));
+
+        if ($run['status'] == 'requires_action') {
+            $this->has_tool_calls = true;
+            $this->tool_call_id = $run['id'];
+            return $run['id'];
+        } else if ($run['status'] == 'completed') {
+            return $run['id'];
+        }
+        return false;
     }
 
     private function execute_request($ch)
@@ -79,79 +308,6 @@ class OpenAIAssistant {
         return $this->execute_request($ch);
     }
 
-    public function create_thread()
-    {
-        $response = $this->send_post_request('/threads');
-
-        if (empty($response['id'])) {
-            throw new Exception('Unable to create a thread');
-        }
-        return $response['id'];
-    }
-
-    public function get_thread($thread_id)
-    {
-        $response = $this->send_get_request("/threads/{$thread_id}");
-
-        if (empty($response['id'])) {
-            throw new Exception('Unable to retrive a thread');
-        }
-        return $response;
-    }
-
-    public function create_message($thread_id, $content, $role = 'user')
-    {
-        // Check if any latest run requires_action
-        // Before adding a new message to the thread
-        $runs = $this->list_runs($thread_id);
-
-        if (count($runs) > 0) {
-            $last_run = $runs[0];
-
-            if ($last_run['status'] == 'requires_action') {
-                $this->has_tool_calls = true;
-                $this->tool_call_id = $last_run['id'];
-                return false;
-            } else {
-                $this->has_tool_calls = false;
-                $this->tool_call_id = null;
-            }
-        }
-
-        $response = $this->send_post_request(
-            "/threads/{$thread_id}/messages",
-            array(
-                'role' => $role,
-                'content' => $content
-            )
-        );
-
-        if (empty($response['id'])) {
-            throw new Exception('Unable to create a message');
-        }
-        return $response['id'];
-    }
-
-    public function get_message($thread_id, $message_id)
-    {
-        $response = $this->send_get_request("/threads/{$thread_id}/messages/{$message_id}");
-
-        if (empty($response['id'])) {
-            throw new Exception('Unable to retrive a message');
-        }
-        return $response;
-    }
-
-    public function list_messages($thread_id)
-    {
-        $response = $this->send_get_request("/threads/{$thread_id}/messages");
-
-        if (empty($response['data'])) {
-            return array();
-        }
-        return $response['data'];
-    }
-
     private function create_run($thread_id, $assistant_id)
     {
         $response = $this->send_post_request(
@@ -175,7 +331,7 @@ class OpenAIAssistant {
         return $response;
     }
 
-    public function list_runs($thread_id)
+    private function list_runs($thread_id)
     {
         $response = $this->send_get_request("/threads/{$thread_id}/runs");
 
@@ -183,100 +339,5 @@ class OpenAIAssistant {
             return array();
         }
         return $response['data'];
-    }
-
-    public function run_thread($thread_id)
-    {
-        // Check if any latest run requires_action
-        // Before creating and running a new thread
-        $runs = $this->list_runs($thread_id);
-
-        if (count($runs) > 0) {
-            $last_run = $runs[0];
-
-            if ($last_run['status'] == 'requires_action') {
-                $this->has_tool_calls = true;
-                $this->tool_call_id = $last_run['id'];
-                return false;
-            } else {
-                $this->has_tool_calls = false;
-                $this->tool_call_id = null;
-            }
-        }
-
-        $run_id = $this->create_run($thread_id, $this->assistant_id);
-
-        do {
-            sleep(5);
-            $run = $this->get_run($thread_id, $run_id);
-        } while (!(
-            $run['status'] == 'completed'
-            || $run['status'] == 'requires_action'
-        ));
-
-        if ($run['status'] == 'requires_action') {
-            $this->has_tool_calls = true;
-            $this->tool_call_id = $run['id'];
-            return $run['id'];
-        } else if ($run['status'] == 'completed') {
-            return $run['id'];
-        }
-        return false;
-    }
-
-    public function execute_tools($thread_id, $execution_id)
-    {
-        $run = $this->get_run($thread_id, $execution_id);
-        $calls = $run['required_action']['submit_tool_outputs']['tool_calls'];
-        $outputs = array();
-
-        foreach ($calls as $call) {
-            $method_name = $call['function']['name'];
-            $method_args = json_decode($call['function']['arguments'], true);
-
-            if (is_callable($method_name)) {
-                $data = call_user_func_array(
-                    $method_name,
-                    $method_args
-                );
-                array_push($outputs, array(
-                    'tool_call_id' => $call['id'],
-                    'output' => json_encode($data)
-                ));
-            } else {
-                throw new Exception("Failed to execute tool: The $method_name you provided is not callable");
-            }
-        }
-        $this->has_tool_calls = false;
-        return $outputs;
-    }
-
-    public function submit_tool_outputs($thread_id, $execution_id, $outputs)
-    {
-        $response = $this->send_post_request(
-            "/threads/{$thread_id}/runs/{$execution_id}/submit_tool_outputs",
-            array('tool_outputs' => $outputs)
-        );
-
-        if (empty($response['id'])) {
-            throw new Exception('Unable to submit tool outputs');
-        }
-
-        do {
-            sleep(5);
-            $run = $this->get_run($thread_id, $response['id']);
-        } while (!(
-            $run['status'] == 'completed'
-            || $run['status'] == 'requires_action'
-        ));
-
-        if ($run['status'] == 'requires_action') {
-            $this->has_tool_calls = true;
-            $this->tool_call_id = $run['id'];
-            return $run['id'];
-        } else if ($run['status'] == 'completed') {
-            return $run['id'];
-        }
-        return false;
     }
 }
